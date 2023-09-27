350bc0a98305fdc5770a0e1d499b282ef05d6100
Muhamed <mickybruna@yandex.ru>
2023-09-27 05:22:14 +0300

    pr-14, №1.2

--- routes/cards.js
+++ routes/cards.js
@@ -13,6 +13,9 @@ const {
 
 // add cards preffix
 router.post('/', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }).unknown(),
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string()
@@ -24,18 +27,27 @@ router.post('/', celebrate({
 router.get('/',  receiveCards);
 
 router.put('/:cardId/likes', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }).unknown(),
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex().required(),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required(),
-  }),
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex().required(),
+    }),
+  }).unknown(),
 }), dislikeCard);
 
 router.delete('/:id', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }).unknown(),
   params: Joi.object().keys({
     id: Joi.string().length(24).hex().required(),
   }),
